fa25e3d0f783583e3f09ba953c1383fa1a392b7d
Abdullah Habes <abdulla096@hotmail.com>
2021-11-17 02:27:08 -0500

    implement counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+console.log('composer-char-counter.js');
+
+const MAX_CHARS = 140;
+const callback = function () {
+  const remainingChars = MAX_CHARS - $(this).val().length;
+
+  $(this).parent().siblings().find('output').text(remainingChars).css('color', '');
+
+  if (remainingChars < 0) {
+    $(this).parent().siblings().find('output').css('color', 'red');
+  }
+}
+$(document).ready(function () {
+
+  $('#tweet-text').on('input', callback)
+});
