d803ca7e0449dad23b27bbf08edbce4df7967b8f
Abdullah Habes <abdulla096@hotmail.com>
2021-11-18 01:51:32 -0500

    creating the tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 console.log('composer-char-counter.js');
 
 const MAX_CHARS = 140;
-const callback = function () {
+const callback = function() {
   const remainingChars = MAX_CHARS - $(this).val().length;
 
   $(this).parent().siblings().find('output').text(remainingChars).css('color', '');
@@ -9,8 +9,8 @@ const callback = function () {
   if (remainingChars < 0) {
     $(this).parent().siblings().find('output').css('color', 'red');
   }
-}
-$(document).ready(function () {
+};
+$(document).ready(function() {
 
-  $('#tweet-text').on('input', callback)
+  $('#tweet-text').on('input', callback);
 });
